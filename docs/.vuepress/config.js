327a335cce34b7ef4ca56b2da9e3c1a250eb1a0e
Nick Bernal <nick@harbourshare.com>
2024-11-14 18:02:59 -0800

    fix base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
+  base: '/Harbour-Enterprises/SuperDoc/',
   bundler: viteBundler(),
   theme: defaultTheme({}),
 
