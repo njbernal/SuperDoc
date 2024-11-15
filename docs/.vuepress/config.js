1b03807f375a042ac719c0693a4e52efb91ba3a1
Nick Bernal <nick@harbourshare.com>
2024-11-14 18:00:48 -0800

    update base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
-  base: '/SuperDoc/',
   bundler: viteBundler(),
   theme: defaultTheme({}),
 
