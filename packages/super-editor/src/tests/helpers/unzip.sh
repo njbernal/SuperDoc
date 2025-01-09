c6b285d90d8c5bc8433114c8010cea620ddd3912
Nick Bernal <nick@harbourshare.com>
2025-01-09 13:00:49 -0800

    add flag for gitignore

--- packages/super-editor/src/tests/helpers/unzip.sh
+++ packages/super-editor/src/tests/helpers/unzip.sh
@@ -12,6 +12,8 @@ fi
 
 # Original file path
 FILE_PATH="$1"
+IS_PUBLIC_SAFE="$2"
+
 echo "🥡 Processing file: $FILE_PATH"
 
 # Extract the file name without the extension
@@ -19,10 +21,11 @@ original_name=$(basename "$FILE_PATH" | sed 's/\.[^.]*$//')
 
 # Remove special characters and replace spaces
 safe_name=$(echo "$original_name" | sed 's/[^a-zA-Z0-9]/_/g' | tr '[:upper:]' '[:lower:]')
-DIR_PATH="./packages/super-editor/src/tests/data/$safe_name"
+BASE_PATH=./packages/super-editor/src/tests/data
+DIR_PATH=$BASE_PATH/$safe_name
 
 if [ -d "$DIR_PATH" ]; then
-  echo "🚫 The directory "$DATA_PATH" already exists... exiting."
+  echo "🚫 The directory $DIR_PATH already exists... exiting."
   exit 0
 fi
 
@@ -38,3 +41,9 @@ unzip -d "$DATA_PATH" "$FILE_PATH"
 ABSOLUTE_DATA_PATH=$(realpath "$DATA_PATH")
 echo "🥡 Data path: $ABSOLUTE_DATA_PATH"
 echo "🥡 Done!"
+
+# Unless the user explicitly tells us that this data is git-safe, we will gitignore it
+if [ "$IS_PUBLIC_SAFE" != "true" ]; then
+  GITIGNORE_PATH=$BASE_PATH/.gitignore
+  echo "$safe_name/" >> "$GITIGNORE_PATH"
+fi
