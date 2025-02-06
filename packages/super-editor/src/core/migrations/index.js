fc1ca051ac01dbefb257d8aa4c50bb285d45f47e
Nick Bernal <nick@harbourshare.com>
2025-02-06 13:31:55 -0800

    Add 0.4.14 to initial migration

--- packages/super-editor/src/core/migrations/index.js
+++ packages/super-editor/src/core/migrations/index.js
@@ -2,10 +2,8 @@ import { migration_after_0_4_14 } from './migration_after_0_4_14';
 
 const DOCUMENT_MIGRATIONS = {
   'initial': migration_after_0_4_14,
-  '0.4.14': migration_after_0_4_14,
 };
 
 export const getNecessaryMigrations = (version) => {
-  if (version === 'initial') return Object.values(DOCUMENT_MIGRATIONS);
-  if (version === '0.4.14') return Object.values(DOCUMENT_MIGRATIONS);
+  if (version === 'initial' || version === '0.4.14') return Object.values(DOCUMENT_MIGRATIONS);
 };
