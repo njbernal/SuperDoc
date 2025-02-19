e9b546eda756e6e244a31ef3de6c273afa0f3e5c
Gabriel Chittolina <gabriel@harbourshare.com>
2025-02-19 16:47:50 -0300

    feat: added initial TS support for super-editor

--- packages/super-editor/src/index.js
+++ packages/super-editor/src/index.js
@@ -1,19 +1,19 @@
 import './style.css';
-import { SuperConverter } from '@core/super-converter/SuperConverter';
-import { getMarksFromSelection } from '@/core/helpers/getMarksFromSelection.js';
-import { getActiveFormatting } from '@/core/helpers/getActiveFormatting.js';
-import { getStarterExtensions, getRichTextExtensions } from '@extensions/index.js';
-import { SuperToolbar } from '@components/toolbar/super-toolbar.js';
-import { DocxZipper, helpers } from '@core/index.js';
-import { Editor } from '@core/Editor.js';
-import { createZip } from '@core/super-converter/zipper.js';
-import { getAllowedImageDimensions } from '@extensions/image/imageHelpers/processUploadedImage.js';
-import SuperEditor from '@components/SuperEditor.vue';
+import { SuperConverter } from './core/super-converter/SuperConverter';
+import { getMarksFromSelection } from './core/helpers/getMarksFromSelection.js';
+import { getActiveFormatting } from './core/helpers/getActiveFormatting.js';
+import { getStarterExtensions, getRichTextExtensions } from './extensions/index.js';
+import { SuperToolbar } from './components/toolbar/super-toolbar.js';
+import { DocxZipper, helpers } from './core/index.js';
+import { Editor } from './core/Editor.js';
+import { createZip } from './core/super-converter/zipper.js';
+import { getAllowedImageDimensions } from './extensions/image/imageHelpers/processUploadedImage.js';
+import SuperEditor from './components/SuperEditor.vue';
 import BasicUpload from './dev/components/BasicUpload.vue';
-import Toolbar from '@components/toolbar/Toolbar.vue';
-import SuperInput from '@components/SuperInput.vue';
-import * as fieldAnnotationHelpers from '@extensions/field-annotation/fieldAnnotationHelpers/index.js';
-import * as trackChangesHelpers from '@extensions/track-changes/trackChangesHelpers/index.js';
+import Toolbar from './components/toolbar/Toolbar.vue';
+import SuperInput from './components/SuperInput.vue';
+import * as fieldAnnotationHelpers from './extensions/field-annotation/fieldAnnotationHelpers/index.js';
+import * as trackChangesHelpers from './extensions/track-changes/trackChangesHelpers/index.js';
 
 /**
  * Exported classes and components.
