92d3e7a860912d52a621231c7b8f7939394820ac
Nick Bernal <nick@harbourshare.com>
2025-03-06 13:25:42 -0800

    Show file ID on websocket events

--- packages/superdoc/src/core/collaboration/collaboration.js
+++ packages/superdoc/src/core/collaboration/collaboration.js
@@ -52,9 +52,9 @@ function createHocuspocusProvider({ config, user, documentId, socket, superdocIn
     document: ydoc,
     name: documentId,
     token: config.token || '',
-    onAuthenticationFailed,
-    onConnect: () => onConnect(superdocInstance),
-    onDisconnect: () => onDisconnect(superdocInstance),
+    onAuthenticationFailed: () => onAuthenticationFailed(documentId),
+    onConnect: () => onConnect(superdocInstance, documentId),
+    onDisconnect: () => onDisconnect(superdocInstance, documentId),
   };
 
   const provider = new HocuspocusProvider(options);
@@ -62,16 +62,16 @@ function createHocuspocusProvider({ config, user, documentId, socket, superdocIn
   return { provider, ydoc };
 }
 
-const onAuthenticationFailed = (data) => {
-  console.warn('🔒 [superdoc] Authentication failed', data);
+const onAuthenticationFailed = (data, documentId) => {
+  console.warn('🔒 [superdoc] Authentication failed', data, 'document', documentId);
 };
 
-const onConnect = (superdocInstance) => {
-  console.warn('🔌 [superdoc] Connected -- ', superdocInstance.config.documents[0]);
+const onConnect = (superdocInstance, documentId) => {
+  console.warn('🔌 [superdoc] Connected -- ', documentId);
 };
 
-const onDisconnect = (superdocInstance) => {
-  console.warn('🔌 [superdoc] Disconnected', superdocInstance.config.documents[0]);
+const onDisconnect = (superdocInstance, documentId) => {
+  console.warn('🔌 [superdoc] Disconnected', documentId);
 };
 
 export { createAwarenessHandler, createProvider };
