d1c38df01e6e5e2de98d0fb113974bfb7660bfbe
Artem Nistuley <artem@harbourshare.com>
2025-03-28 15:59:44 +0200

    return toolbar accept/reject buttons

--- packages/super-editor/src/components/toolbar/defaultItems.js
+++ packages/super-editor/src/components/toolbar/defaultItems.js
@@ -821,8 +821,8 @@ export const makeDefaultItems = (superToolbar, isDev = false, windowWidth, role,
 
     // Dev - tracked changes
     // toggleTrackChanges,
-    // acceptTrackedChangeBySelection,
-    // rejectTrackedChangeOnSelection,
+    acceptTrackedChangeBySelection,
+    rejectTrackedChangeOnSelection,
     // toggleTrackChangesOriginal,
     // toggleTrackChangesFinal,
 
