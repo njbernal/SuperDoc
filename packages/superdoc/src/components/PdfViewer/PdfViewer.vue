958b09d7b79fb94a3d0ae2ced4376e7a50117cce
Nick Bernal <nick@harbourshare.com>
2025-04-10 07:03:13 -0700

    Revert "Allow passing in pdfjs worker url from user"
    This reverts commit b88cfc244ca54bb3d9b98e608084eb0f8449741c.

--- packages/superdoc/src/components/PdfViewer/PdfViewer.vue
+++ packages/superdoc/src/components/PdfViewer/PdfViewer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.mjs';
 import * as pdfjsViewer from 'pdfjs-dist/web/pdf_viewer';
+import * as pdfWorkerSrc from 'pdfjs-dist/build/pdf.worker.min.mjs';
 import { range } from './helpers/range.js';
 import { NSpin } from 'naive-ui';
 
@@ -42,8 +43,8 @@ const getOriginalPageSize = (page) => {
 };
 
 async function initPdfWorker() {
-  if (!proxy?.$superdoc?.config?.pdfjsWorkerSrcUrl) return;
-  pdfjsLib.GlobalWorkerOptions.workerSrc = proxy.$superdoc.config.pdfjsWorkerSrcUrl
+  const workerUrl = URL.createObjectURL(new Blob([pdfWorkerSrc], { type: 'text/javascript' }));
+  pdfjsLib.GlobalWorkerOptions.workerSrc = workerUrl;
 }
 
 async function initPdfLayer(arrayBuffer) {
