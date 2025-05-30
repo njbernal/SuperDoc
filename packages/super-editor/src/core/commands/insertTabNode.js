d3b7b9d3c0ae4d7b60f1920a184404e3a5d362fa
Nick Bernal <nick@harbourshare.com>
2025-05-29 20:34:52 -0700

    Fix tab press in html editor

--- packages/super-editor/src/core/commands/insertTabNode.js
+++ packages/super-editor/src/core/commands/insertTabNode.js
@@ -1,8 +1,24 @@
-//prettier-ignore
-export const insertTabNode = () => ({ tr, state }) => {
-  let newPos = tr.selection.from;
-  const node = state.schema.nodes.tab.create();
-  tr.insert(newPos, node);
-  
+import { TextSelection } from 'prosemirror-state';
+
+export const insertTabCharacter = ({ tr, state, dispatch }) => {
+  const { from } = tr.selection;
+  const tabText = state.schema.text('\t');
+
+  tr = tr.replaceSelectionWith(tabText);
+  tr = tr.setSelection(TextSelection.create(tr.doc, from + 1));
+
+  if (dispatch) dispatch(tr);
+  return true;
+};
+
+export const insertTabNode = () => ({ tr, state, dispatch }) => {
+  const newPos = tr.selection.from;
+  const tabNode = state.schema?.nodes?.tab?.create();
+
+  // If tab node isn't defined, fallback to tab character
+  if (!tabNode) return insertTabCharacter({ tr, state, dispatch });
+
+  tr.insert(newPos, tabNode);
+  if (dispatch) dispatch(tr);
   return true;
 };
