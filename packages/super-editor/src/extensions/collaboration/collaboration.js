266095e22e054470e4447c29ffcb15a3d475a206
Nick Bernal <nick@harbourshare.com>
2025-05-30 11:02:03 -0700

    Fix collaboration.js

--- packages/super-editor/src/extensions/collaboration/collaboration.js
+++ packages/super-editor/src/extensions/collaboration/collaboration.js
@@ -1,6 +1,5 @@
 import { Extension } from '@core/index.js';
 import { PluginKey } from 'prosemirror-state';
-import { prosemirrorToYDoc } from 'yjs';
 import { ySyncPlugin, yUndoPlugin, yUndoPluginKey, undo, redo, prosemirrorToYDoc } from 'y-prosemirror';
 
 export const CollaborationPluginKey = new PluginKey('collaboration');
