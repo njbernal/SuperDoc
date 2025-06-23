f020e9ec8daab20f5ba06450982f96c8059aa8a3
Artem Nistuley <artem@harbourshare.com>
2025-06-23 17:48:07 +0300

    fix pdf annotation font-size

--- packages/superdoc/src/components/HrbrFieldsLayer/CheckboxField.vue
+++ packages/superdoc/src/components/HrbrFieldsLayer/CheckboxField.vue
@@ -16,17 +16,35 @@ const props = defineProps({
     type: String,
     required: true,
   },
+  styleOverride: {
+    type: Object,
+    required: false,
+    default: () => ({}),
+  },
 });
 
 const getValue = computed(() => {
   const match = props.field.options.find((o) => o.annotationId.includes(props.optionId));
   return match?.checked;
 });
+
+const getPreviewStyle = computed(() => {
+  const borderWidth = 2;
+  const width = (Number.parseFloat(props.styleOverride?.coordinates?.minWidth || 0) - borderWidth) + 'px';
+  const height = (Number.parseFloat(props.styleOverride?.coordinates?.minHeight || 0) - borderWidth) + 'px';
+  const fontSize = parseFloat(width) + 'pt';
+  return {
+    width,
+    height,
+    fontSize,
+  };
+});
 </script>
 
 <template>
   <div class="checkbox-container">
-    <n-checkbox :checked="getValue" :disabled="!props.isEditing"></n-checkbox>
+    <n-checkbox v-if="props.isEditing" :checked="getValue" :disabled="!props.isEditing"></n-checkbox>
+    <div v-else class="checkbox-preview" :style="getPreviewStyle">{{ getValue ? 'x' : '' }}</div>
   </div>
 </template>
 
@@ -36,6 +54,16 @@ const getValue = computed(() => {
   align-items: center;
   justify-content: center;
   width: fit-content;
-  margin-top: 5px;
+  width: 100%;
+  height: 100%;
+}
+
+.checkbox-preview {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.2;
+  width: 100%;
+  height: 100%;
 }
 </style>
