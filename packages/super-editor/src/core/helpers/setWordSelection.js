b5209c7b31e8198ab38c051345a5f01ec220738f
Nick Bernal <nick@harbourshare.com>
2025-07-23 09:33:52 -0700

    fix: setWordSelection if no word selected

--- packages/super-editor/src/core/helpers/setWordSelection.js
+++ packages/super-editor/src/core/helpers/setWordSelection.js
@@ -4,6 +4,7 @@ import { TextSelection } from 'prosemirror-state';
 export const setWordSelection = (view, pos) => {
   const { state, dispatch } = view;
   const word = findWordBounds(state.doc, pos);
+  if (!word) return;
   const tr = state.tr.setSelection(TextSelection.create(state.doc, word.from, word.to));
   dispatch(tr);
 };
