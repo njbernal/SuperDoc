1c7be6904f809477874092f5e1a1aa59afc07b93
Fatima AlSaadeh <fatima@harbourshare.com>
2025-07-30 11:01:38 +0100

    fix: har-10195 highlight tracked changes comment

--- packages/superdoc/src/components/CommentsLayer/CommentDialog.vue
+++ packages/superdoc/src/components/CommentsLayer/CommentDialog.vue
@@ -125,6 +125,13 @@ const setFocus = () => {
 const handleClickOutside = (e) => {
   if (e.target.classList.contains('n-dropdown-option-body__label')) return;
   if (e.target.classList.contains('sd-editor-comment-highlight')) return;
+  if (e.target.classList.contains('sd-editor-tracked-change-highlight')) return;
+  if (e.target.classList.contains('track-insert')) return;
+  if (e.target.classList.contains('track-insert-dec')) return;
+  if (e.target.classList.contains('track-delete')) return;
+  if (e.target.classList.contains('track-delete-dec')) return;
+  if (e.target.classList.contains('track-format')) return;
+  if (e.target.classList.contains('track-format-dec')) return;
   if (activeComment.value === props.comment.commentId) {
     floatingCommentsOffset.value = 0;
     emit('dialog-exit');
